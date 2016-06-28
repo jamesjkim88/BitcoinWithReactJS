874849ca106f6d371370355b2ed0f6e01454fffb
James Kim <jamesjkim8806@gmail.com>
2016-06-27 22:00:44 -0700

    added ajax for amount

--- src/components/Amount.js
+++ src/components/Amount.js
@@ -1,21 +1,40 @@
 import React from "react";
+import AmountActions from "../actions/AmountActions";
 
 export default class Amount extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            amount: 0
+            amount: 0,
+            currency: "USD"
         }
     }
 
-    onInputChange(amount){
+    // onInputChange(amount){
+    //     this.setState({amount});
+    //     console.log(amount);
+    // }
+    
+    ajaxCall(amount, currency){
         this.setState({amount});
         console.log(amount);
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://montanaflynn-bitcoin-exchange-rate.p.mashape.com/prices/spot_rate?currency=USD&mashape-key=4gN0HY6RLTmshjwm5OsfyaRS5PBLp1yxVacjsnlOdcza7YSwGS');
+        xhr.addEventListener('readystatechange', function () {
+                if (xhr.readyState === 4) {
+                    console.log("data: ", JSON.parse(xhr.responseText));
+                    var data = JSON.parse(xhr.responseText);
+                    var total = data.amount * amount;
+                    console.log(total.toFixed(2));
+                }
+                });
+            xhr.send();
+            console.log('Made the call');
     }
 
     render(){
         return(
-            <input className="textBox" id="amount" type="text" value={this.state.amount} onChange={(evt) => this.onInputChange(evt.target.value)} />
+            <input className="textBox" id="amount" type="text" value={this.state.amount} onChange={(evt) => this.ajaxCall(evt.target.value, evt.nativeEvent.target.text)} />
         )
     }
 }
