a6927f34511b9a3a636539682a6a2711cf78eaf4
James Kim <jamesjkim8806@gmail.com>
2016-06-27 23:17:24 -0700

    ajax call to differenct currency, label not changing though

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,14 +1,35 @@
 import React, {Component} from "react";
+import Select from "react-select";
+
+var options = [
+    { value: 'one', label: 'USD' },
+    { value: 'two', label: 'KRW' }
+];
+
+function logChange(val) {
+    console.log(val);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://montanaflynn-bitcoin-exchange-rate.p.mashape.com/prices/spot_rate?currency=' + val.label + '&mashape-key=4gN0HY6RLTmshjwm5OsfyaRS5PBLp1yxVacjsnlOdcza7YSwGS');
+    xhr.addEventListener('readystatechange', function () {
+        console.log(xhr.readyState);
+        if (xhr.readyState === 4) {
+            console.log("data: ", JSON.parse(xhr.responseText));
+        }
+    });
+    xhr.send();
+    console.log('Made the call');
+}
 
 export default class Currency extends Component{
     render(){
         return(
-            <select id="currencyBox">
-                <option disabled selected value="">Select Currency</option>
-                <option selected value="USD">USD</option>
-                <option value="AUD">AUD</option>
-                <option value="KRW">KRW</option>
-            </select>
+            <Select
+                name="form-field-name"
+                value="one"
+                options={options}
+                onChange={logChange}
+            />
         )
     }
 }
+
