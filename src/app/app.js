d45bf15ccdfbc4611b8d6cf7c15815c6e8d7fbcd
James Kim <jamesjkim8806@gmail.com>
2016-07-07 23:18:08 -0700

    let there be render

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {createStore, applyMiddleware} from "redux";
+import ReduxPromise from "redux-promise";
+import reducers from "../reducers";
 import Layout from "../components/Layout";
 
 const app = document.getElementById('app');
-ReactDOM.render(<Layout/>, app);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+    <Layout/>
+        </Provider> , app);
