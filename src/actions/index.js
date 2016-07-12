9a4458c3f758f235d1907d99beccb0481b9d2981
James Kim <jamesjkim8806@gmail.com>
2016-07-11 21:48:11 -0700

    ajax tweaks

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const rootUrl = "https://montanaflynn-bitcoin-exchange-rate.p.mashape.com/prices/spot_rate";
+const rootUrl = "https://montanaflynn-bitcoin-exchange-rate.p.mashape.com/prices/buy";
 const apiKey = "4gN0HY6RLTmshjwm5OsfyaRS5PBLp1yxVacjsnlOdcza7YSwGS";
 
-const FETCH_BITCOIN = "FETCH_BITCOIN";
+export const FETCH_BITCOIN = "FETCH_BITCOIN";
 
-export function fetchBitcoin(currency) {
-    const url = `${rootUrl}?currency=${currency}&mashape-key=${apiKey}`;
+export function fetchBitcoin(btc) {
+    const url = `${rootUrl}?qty=${btc}&mashape-key=${apiKey}`;
     const request = axios.get(url);
     return{
         type: FETCH_BITCOIN,
         payload: request
     };
-};
+}
--- src/containers/Converted.js
+++ src/containers/Converted.js
@@ -5,11 +5,22 @@ class BitcoinConverted extends Component{
 
     renderBTCConverted(data){
         console.log("data: ", data);
+        console.log("fees: ", data.fees);
+        const amount = data.amount;
+        const fees = data.fees.map((bitcoin,i) => bitcoin.coinbase.amount);
+        return(
+            <li key={i}>{amount}, {fees}</li>
+        )
     }
 
     render(){
+        console.log(this.props);
         return(
-            <h1>{this.renderBTCConverted.bind(this)}</h1>
+            <div>
+                <ul>
+                    test: {this.props.bitcoin.map(this.renderBTCConverted)}
+                </ul>
+            </div>
         )
     }
 }
